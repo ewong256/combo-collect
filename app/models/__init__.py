49ac7a27cd7f214de95297d36b18a0cb07b4a5d5
Ethan Wong <wngethn256@gmail.com>
2025-02-28 11:43:45 -0500

    Tables Migrated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
+from .clip import Clip
+from .collection import Collection
+from .collection_clips import collection_clips
 from .db import environment, SCHEMA
